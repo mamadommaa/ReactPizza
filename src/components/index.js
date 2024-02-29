b468c0ad737a553ceb131bf206ba370ab3da4708
unknown <mamadommaa@gmail.com>
2024-03-01 01:29:02 +0300

    add Routing and setting app structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,3 @@
-// import Button from "./Button";
-// import Header from "./Header";
-// import Categories from "./Categories";
-
-// export { Button, Header, Categories };
-
 export { default as Button } from "./Button.jsx";
 export { default as Header } from "./Header.jsx";
 export { default as Categories } from "./Categories.jsx";
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { BrowserRouter } from "react-router-dom";
+
 import "./scss/app.scss";
 
 import App from "./App";
@@ -9,7 +11,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
 
