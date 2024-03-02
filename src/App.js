3fd49e789da5d8c441fdac2d3dbf56191cebdaa2
unknown <mamadommaa@gmail.com>
2024-03-02 05:31:25 +0300

    add db.json and create + setting PizzaBlock.jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { Route, Routes } from "react-router";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+    const [pizzas, setPizzas] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/db.json")
+            .then((response) => response.json())
+            .then((json) => setPizzas(json.pizzas));
+    }, []);
+
+    // console.log(pizzas);
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Home pizzas={pizzas} />} />
                     <Route path="/cart" element={<Cart />} />
                 </Routes>
             </div>
